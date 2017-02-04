4de0e6bda68c1d97df03eb84971ac604ffa836ca
Pooneet Thaper <pooneetthaper@gmail.com>
2017-02-04 13:24:43 -0500

    retrained and updated models, played around with face detection parameters, added sample outputs

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -18,7 +18,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.2,
+    scaleFactor=1.3,
     minNeighbors=5,
     minSize=(30, 30)
     #flags = cv2.CV_HAAR_SCALE_IMAGE
